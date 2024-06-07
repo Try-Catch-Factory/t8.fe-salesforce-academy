a1c630e27a889c6110b1ef67096458bdb7ab2d58
Elkin Gabriel <latinohablantes@gmail.com>
2024-06-07 09:07:43 -0500

    colors's name tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,18 +9,20 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        '50': '#fefce8',
-        '100': '#fdf8c4',
-        '200': '#fcef8c',
-        '300': '#fadd4a',
-        '400': '#f7c918',
-        '500': '#f2b90c',
-        '600': '#c78807',
-        '700': '#9f6109',
-        '800': '#834c10',
-        '900': '#703f13',
-        '950': '#412007',
-    },
+        'pyellow': {
+          '50': '#fefce8',
+          '100': '#fdf8c4',
+          '200': '#fcef8c',
+          '300': '#fadd4a',
+          '400': '#f7c918',
+          '500': '#f2b90c',
+          '600': '#c78807',
+          '700': '#9f6109',
+          '800': '#834c10',
+          '900': '#703f13',
+          '950': '#412007',
+        },  
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
